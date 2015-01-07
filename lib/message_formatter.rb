1327a31af6eb8a9f40b353b813d5f1843198a57a
ruok5 <brad@fyvm.org>
2015-01-07 11:37:37 -0800

    remove URL shortening

--- lib/message_formatter.rb
+++ lib/message_formatter.rb
@@ -7,7 +7,7 @@ class MessageFormatter
     info = MultiJson.load(json)
     branch = info['ref'].split('/').last
     info['commits'].each do |ci|
-      url = self.short_url(ci['url'])
+      url = ci['url']
       ci_title = ci['message'].lines.first.chomp
       msg = "[#{info['repository']['name'].capitalize}(#{branch})] #{ci['author']['name']} | #{ci_title} | #{url}"
       msgs << msg
